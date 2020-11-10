e6f4d8825644b881edc51528acb333eb48a8fdc6
Zhu Mei <meizhu812@outlook.com>
2020-11-10 16:44:36 +0800

    completed practice2

--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,18 @@
-const inject
-export { inject };
+const inject = (originals, injections) => {
+    injections.sort((a, b) => a.index - b.index);
+    const result = [];
+
+    injections.reduce((partial, injection, index) => {
+        const currentOriginalsCount = partial.length - index;
+        const originalsSlice = originals.slice(currentOriginalsCount, injection.index);
+        partial.push(...originalsSlice, injection.content);
+        return partial;
+    }, result);
+
+    const tailStartIndex = injections[injections.length - 1].index;
+    const tails = originals.slice(tailStartIndex, originals.length);
+
+    result.push(...tails);
+    return result;
+}
+export {inject};
