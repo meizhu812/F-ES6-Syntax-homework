00657197bb1cedd2ab406562ac1ce9c47e8d70ba
Zhu Mei <meizhu812@outlook.com>
2020-11-10 16:44:27 +0800

    completed practice1

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,8 @@
-const parseData
-export { parseData };
+const parseData = input => {
+    const {data: dataArray, column: columnsArray} = input;
+    return dataArray.map(data => data.reduce((resultObj, value, index) => {
+        resultObj[columnsArray[index].name] = value;
+        return resultObj;
+    }, {}));
+}
+export {parseData};
